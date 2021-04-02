92a07afd733cdeb960d0326e9226e3dcc878ab48
wangYP <wb-wyp737365@antfin.com>
2021-04-02 16:01:23 +0800

    chore: 修改测试数据

--- src/data.js
+++ src/data.js
@@ -31,9 +31,9 @@ module.exports.testData = [
         title: '0-1',
         key: '0-1',
         children: [
-            { title: '0-1-0-0', key: '0-1-0-0' },
-            { title: '0-1-0-1', key: '0-1-0-1' },
-            { title: '0-1-0-2', key: '0-1-0-2' },
+            { title: '0-1-0', key: '0-1-0' },
+            { title: '0-1-1', key: '0-1-1' },
+            { title: '0-1-2', key: '0-1-2' },
         ],
     },
     {

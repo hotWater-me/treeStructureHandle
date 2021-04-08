3cac0a8e1d29d4de040ecf0d09455902bc2f98d5
wangYP <wb-wyp737365@antfin.com>
2021-04-08 17:58:54 +0800

    fea: 新增ts声明文件

---
+++ src/main.d.ts
@@ -0,0 +1,68 @@
+interface uniteObject {
+  [key: string]: any;
+  children?: uniteObject[];
+}
+
+interface arrToTreeDataType {
+  id: number;
+  parentId: number;
+}
+
+interface objectType {
+  [key: string]: string;
+}
+
+declare class RecursionHalle {
+  getName(id: number): string   
+}
+
+export declare namespace treeRecursionHalle {
+  /** 获取树结构数据某一个键值 */
+  function convertData(testData: uniteObject[], selEle: string): string[];
+
+  /** 将一维数组转化为树结构类型数据 */
+  function arrToTreeData(arrToTreeDataType): uniteObject;
+
+  /** 获得树结构类型数据的路径 */
+  function nodePath(
+    originData: uniteObject[],
+    keyName: string,
+    customSymbol: string
+  ): string[];
+
+  /** 获取节点的子节点 */
+  function getChildNode(
+    originData: uniteObject[],
+    keyName: string,
+    nodeName: string
+  ): objectType;
+
+  /** 为每个节点插入属性 */
+  function insertAttr(
+    originData: uniteObject[],
+    insertObj: objectType
+  ): uniteObject;
+
+  /** 为节点替换键名 */
+  function replaceKeyName(
+    originData: uniteObject[],
+    replaceObj: objectType
+  ): uniteObject;
+
+  /** 获得节点的个数 */
+  function getnodeCount(testData: uniteObject[]): number;
+
+  /** 一维化数据 */
+  function downGradeData(
+    originData: uniteObject[],
+    idValue: string,
+    parentValue: string,
+    accTemp: uniteObject[]
+  ): number;
+
+  /** 去重并去除undefined */
+  function delUndefined(inCome: string[]): string[];
+
+  /** 对象替换键名 */
+  function renameKeys(keysMap: objectType, obj: objectType): objectType;
+}
